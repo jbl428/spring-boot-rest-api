d0f41484d01f93bf110f55cd01a67c2bdd6f35f6
ujusy <lsyeon1997@ajou.ac.kr>
2022-01-21 16:24:44 +0900

    Initial commit

---
+++ src/main/kotlin/com/uju/springbootrestapi/SpringBootRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.uju.springbootrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootRestApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootRestApiApplication>(*args)
+}
