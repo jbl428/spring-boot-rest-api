b4dd39bdb97ae58bfd38f235cdfe9af643877682
ujusy <lsyeon1997@ajou.ac.kr>
2022-01-23 23:59:20 +0900

    new: add Memeber domain

--- uju-domain/src/main/kotlin/com/uju/springbootrestapi/Member/Member.kt
+++ uju-domain/src/main/kotlin/com/uju/springbootrestapi/Member/Member.kt
@@ -1,18 +1,23 @@
 package com.uju.springbootrestapi.Member
 
+import com.uju.springbootrestapi.Adderss.Address
+import com.uju.springbootrestapi.Order.Order
 import org.springframework.transaction.annotation.Transactional
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
-@Transactional
 class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     var id: Long = 0
 
     var username: String? = null
+
+    @Embedded
+    lateinit var address: Address
+
+    @OneToMany(mappedBy = "member")
+    var orders: MutableList<Order> = mutableListOf()
 }
