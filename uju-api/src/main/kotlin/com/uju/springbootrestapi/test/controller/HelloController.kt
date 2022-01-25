1dd2e4a7d9c5fb236ebd36b2f0c11626871047d9
ujusy <lsyeon1997@ajou.ac.kr>
2022-01-25 23:40:50 +0900

    chore: update test controller

--- uju-api/src/main/kotlin/com/uju/springbootrestapi/test/controller/HelloController.kt
+++ uju-api/src/main/kotlin/com/uju/springbootrestapi/test/controller/HelloController.kt
@@ -3,13 +3,15 @@ package com.uju.springbootrestapi.test.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class HelloController {
 
-    @GetMapping("hello")
+
+    @RequestMapping("/")
     fun hello(model: Model): String {
-        model.addAttribute("data", "hello!!!")
-        return "hello"
+
+        return "home"
     }
 }
