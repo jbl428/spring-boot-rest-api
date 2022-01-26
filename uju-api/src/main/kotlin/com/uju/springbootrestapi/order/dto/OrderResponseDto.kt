48990e41e97d3917d004eb46e31cb24bfad58efc
ujusy <lsyeon1997@ajou.ac.kr>
2022-01-26 13:06:30 +0900

    fix: orderResponseDto

--- uju-api/src/main/kotlin/com/uju/springbootrestapi/order/dto/OrderResponseDto.kt
+++ uju-api/src/main/kotlin/com/uju/springbootrestapi/order/dto/OrderResponseDto.kt
@@ -1,16 +1,21 @@
 package com.uju.springbootrestapi.order.dto
 
-import com.uju.springbootrestapi.order.Order
+import com.uju.springbootrestapi.adderss.Address
 import com.uju.springbootrestapi.order.OrderStatus
 import com.uju.springbootrestapi.orderItem.OrderItem
-import org.apache.tomcat.jni.Address
 import java.time.LocalDateTime
 
 data class OrderResponseDto(
-    var orderId: Order,
+    var orderId: Long,
     var name: String,
     var orderDate: LocalDateTime,
     var orderStatus: OrderStatus,
     var address: Address,
     var orderItems: List<OrderItem>
+)
+
+data class OrderItemDto(
+    var name: String,
+    var orderPrice: Int,
+    var count: Int
 )
