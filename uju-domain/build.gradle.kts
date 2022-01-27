f453f4656999b15afddc104706609f30387cf5f8
ujusy <lsyeon1997@ajou.ac.kr>
2022-01-27 13:52:54 +0900

    build: data-jpa 전체 bulid로 옮겨서 삭제 (entityManager 사용하기 위해)

--- uju-domain/build.gradle.kts
+++ uju-domain/build.gradle.kts
@@ -9,6 +9,5 @@ tasks {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-test:2.6.3")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.6.3")
     runtimeOnly("com.h2database:h2:1.4.200")
 }
